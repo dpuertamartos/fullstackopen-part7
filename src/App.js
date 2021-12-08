69eee5e39e4553c41c56620d0baf0de242d424ab
David Puerta Martos <dpuerta@usal.es>
2021-12-08 10:42:36 +0100

    7.5

--- src/App.js
+++ src/App.js
@@ -89,6 +89,13 @@ const CreateNew = (props) => {
     history.push('/')
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -106,6 +113,7 @@ const CreateNew = (props) => {
           <input {...info} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
